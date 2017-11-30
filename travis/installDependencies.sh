d34aa5a9e338f0d9860de73fc898007411baa43b
PBerger <philipp.berger@rwth-aachen.de>
2017-11-30 20:25:15 +0100

    Fucking travis.

--- travis/installDependencies.sh
+++ travis/installDependencies.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-echo "0"
-pwd
+CURRENT_DIR=`pwd`
 
 if [ $TRAVIS_OS_NAME = linux ]; then
-  CURRENT_DIR=`pwd`
   sudo add-apt-repository ppa:ondrej/php -y
   sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
   if [ -n "${QT_PPA}" ]
@@ -22,22 +20,14 @@ if [ $TRAVIS_OS_NAME = linux ]; then
   sudo apt-get install -yq g++-6
   sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-6 90
 
-  echo "1"
-  pwd
   mkdir build-dependencies
   pushd build-dependencies
-  echo "2"
-  pwd
 
   git clone https://github.com/sqlcipher/sqlcipher.git
   pushd sqlcipher && ./configure --enable-tempstore=yes CFLAGS="-DSQLITE_HAS_CODEC" --prefix=/usr && make && sudo make install && popd
-  echo "3"
-  pwd
 
   git clone https://github.com/blizzard4591/qt5-sqlcipher.git
   pushd qt5-sqlcipher && mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr && make && sudo make install && popd
-  echo "4"
-  pwd
   popd
 
   if [ "${DEPLOY_APPIMAGE}" = "true" ]
@@ -45,11 +35,6 @@ if [ $TRAVIS_OS_NAME = linux ]; then
     sudo wget -c "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage" -O /usr/local/bin/linuxdeployqt
     sudo chmod a+x /usr/local/bin/linuxdeployqt
   fi
-  echo "5"
-  pwd
-  cd $CURRENT_DIR
-  echo "6"
-  pwd
 else
   # OS X
   brew install libsodium
@@ -61,6 +46,5 @@ else
   brew link --force sqlcipher
   brew link --force qrencode
 fi
-echo "7"
-pwd
 
+cd $CURRENT_DIR
