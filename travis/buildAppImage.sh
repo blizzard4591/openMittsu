1ab845fee339249998648898ea2c7d88e32c917c
PBerger <philipp.berger@rwth-aachen.de>
2017-12-02 21:23:54 +0100

    Tried to fix install on linux and mac os.

--- travis/buildAppImage.sh
+++ travis/buildAppImage.sh
@@ -6,22 +6,27 @@ set -eufv -o pipefail
 # build AppImage
 if [ "${DEPLOY_APPIMAGE-}" = "true" ]
 then
-  if [ $TRAVIS_OS_NAME = linux ]; then
-    mkdir -p ./build/openMittsu.AppDir/usr/bin
-	mkdir -p ./build/openMittsu.AppDir/usr/lib/x86_64-linux-gnu/qt5/plugins/sqldrivers
-	mkdir -p ./build/openMittsu.AppDir/usr/plugins/sqldrivers
-    cp ./build/openMittsu ./build/openMittsu.AppDir/usr/bin
-    cp ./build/openMittsuTests ./build/openMittsu.AppDir/usr/bin
-    cp ./build/openMittsuVersionInfo ./build/openMittsu.AppDir/usr/bin
-    cp ./resources/icon.svg ./build/openMittsu.AppDir/openmittsu.svg
-    cp ./travis/openmittsu.desktop ./build/openMittsu.AppDir/openmittsu.desktop
-	cp /usr/lib/x86_64-linux-gnu/qt5/plugins/sqldrivers/libqsqlcipher.so ./build/openMittsu.AppDir/usr/lib/x86_64-linux-gnu/qt5/plugins/sqldrivers
-	cp /usr/lib/x86_64-linux-gnu/qt5/plugins/sqldrivers/libqsqlcipher.so ./build/openMittsu.AppDir/usr/plugins/sqldrivers
-    LD_LIBRARY_PATH="" linuxdeployqt ./build/openMittsu.AppDir/usr/bin/openMittsu -appimage
-	echo "AppImages:"
-    find . -type f -name '*.AppImage'
-  else
-    echo "Allll the files:"
-    find . -type f
-  fi
+	if [ $TRAVIS_OS_NAME = linux ]; then
+		mkdir -p ./build/openMittsu.AppDir/usr/bin
+		mkdir -p ./build/openMittsu.AppDir/usr/lib/x86_64-linux-gnu/qt5/plugins/sqldrivers
+		mkdir -p ./build/openMittsu.AppDir/usr/plugins/sqldrivers
+		cp ./build/openMittsu ./build/openMittsu.AppDir/usr/bin
+		cp ./build/openMittsuTests ./build/openMittsu.AppDir/usr/bin
+		cp ./build/openMittsuVersionInfo ./build/openMittsu.AppDir/usr/bin
+		cp ./resources/icon.svg ./build/openMittsu.AppDir/openmittsu.svg
+		cp ./travis/openmittsu.desktop ./build/openMittsu.AppDir/openmittsu.desktop
+		echo "Deploy dir in AppImage creation is $DEPLOY_DIR"
+		cp $DEPLOY_DIR/libqsqlcipher.so ./build/openMittsu.AppDir/usr/lib/x86_64-linux-gnu/qt5/plugins/sqldrivers
+		cp $DEPLOY_DIR/libqsqlcipher.so ./build/openMittsu.AppDir/usr/plugins/sqldrivers
+		LD_LIBRARY_PATH="" linuxdeployqt ./build/openMittsu.AppDir/usr/bin/openMittsu -appimage
+		echo "AppImages:"
+		find . -type f -name '*.AppImage'
+	else
+		echo "Allll the files:"
+		find . -type f
+		/usr/local/opt/qt5/bin/macdeployqt openmittsu.app -always-overwrite -verbose=2
+		curl -o /tmp/macdeployqtfix.py https://raw.githubusercontent.com/aurelien-rainone/macdeployqtfix/master/macdeployqtfix.py
+		python /tmp/macdeployqtfix.py ./openmittsu.app/Contents/MacOS/openmittsu /usr/local/Cellar/qt/$QT_VERSION/
+		/usr/local/opt/qt5/bin/macdeployqt openmittsu.app -dmg -verbose=2
+	fi
 fi
