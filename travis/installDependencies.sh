8ea13686d23e3fc8828e341767af2623acc8dcdb
Philipp Berger <berger@cs.rwth-aachen.de>
2018-10-17 16:02:27 +0200

    Fixed some issues with the travis install dependencies.

--- travis/installDependencies.sh
+++ travis/installDependencies.sh
@@ -6,22 +6,26 @@ export DEPLOY_DIR=$CURRENT_DIR/deploy
 echo "Deploy directory is: $DEPLOY_DIR"
 
 if [ $TRAVIS_OS_NAME = linux ]; then
+	# LibSodium etc
 	sudo add-apt-repository ppa:ondrej/php -y
+	# Current GCC
 	sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 	if [ -n "${QT_PPA}" ]
 	then
-	sudo add-apt-repository "${QT_PPA}" -y
-	sudo apt-get update -q
-	sudo apt-get install -y "${QT_BASE}3d" "${QT_BASE}base" "${QT_BASE}graphicaleffects" "${QT_BASE}imageformats" "${QT_BASE}multimedia" "${QT_BASE}svg" "${QT_BASE}tools" "${QT_BASE}translations" "${QT_BASE}webengine"
+		sudo add-apt-repository "${QT_PPA}" -y
+		sudo apt-get update -q
+		sudo apt-get install -y "${QT_BASE}3d" "${QT_BASE}base" "${QT_BASE}graphicaleffects" "${QT_BASE}imageformats" "${QT_BASE}multimedia" "${QT_BASE}svg" "${QT_BASE}tools" "${QT_BASE}translations" "${QT_BASE}webengine"
 	source "/opt/${QT_BASE}/bin/${QT_BASE}-env.sh"
 	else
-	sudo apt-get update -q
-	sudo apt-get install -y qt5-default qttools5-dev-tools libqt5core5a libqt5gui5 libqt5multimedia5 libqt5multimedia5-plugins libqt5sql5 libqt5sql5-sqlite libqt5widgets5 qt5-qmake qtbase5-dev qtbase5-dev-tools qtmultimedia5-dev qtbase5-dev qtbase5-private-dev
+		sudo apt-get update -q
+		sudo apt-get install -y qt5-default qttools5-dev-tools libqt5core5a libqt5gui5 libqt5multimedia5 libqt5multimedia5-plugins libqt5sql5 libqt5sql5-sqlite libqt5widgets5 qt5-qmake qtbase5-dev qtbase5-dev-tools qtmultimedia5-dev qtbase5-dev qtbase5-private-dev
 	fi
 
-	sudo apt-get install -yq  libqrencode-dev git g++ libssl-dev make cmake pkgconf libsodium-dev libsodium18 zlib1g zlib1g-dev openssl
-	sudo apt-get install -yq g++-6
-	sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-6 90
+	# TclSH: Required by SqlCipher
+	# Mesa: Provides GL/gl.h for openMittsu
+	sudo apt-get install -yq  libqrencode-dev git g++ libssl-dev make cmake3 pkgconf libsodium-dev zlib1g zlib1g-dev openssl tclsh tcl-dev mesa-common-dev
+	sudo apt-get install -yq g++-8
+	sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 90
 
 	which $CC
 	which $CXX
@@ -34,7 +38,7 @@ if [ $TRAVIS_OS_NAME = linux ]; then
 
 		git clone https://github.com/sqlcipher/sqlcipher.git
 		pushd sqlcipher
-			./configure --enable-tempstore=yes CFLAGS="-DSQLITE_HAS_CODEC" --prefix=/usr
+			./configure --enable-tempstore=yes CFLAGS="-DSQLITE_HAS_CODEC -DSQLITE_ENABLE_JSON1 -DSQLITE_ENABLE_FTS3 -DSQLITE_ENABLE_FTS3_PARENTHESIS -DSQLITE_ENABLE_FTS5 -DSQLITE_ENABLE_COLUMN_METADATA" --prefix=/usr
 			make
 			sudo make install
 		popd
