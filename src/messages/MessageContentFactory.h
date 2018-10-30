88704b559d1bce34b2b622645c08cf5073bb3790
Philipp Berger <berger@cs.rwth-aachen.de>
2018-10-30 19:15:09 +0100

    Fixed missing virtual destructor.

--- src/messages/MessageContentFactory.h
+++ src/messages/MessageContentFactory.h
@@ -10,12 +10,16 @@ namespace openmittsu {
 
 		class MessageContentFactory {
 		public:
+			virtual ~MessageContentFactory() {}
+
 			virtual MessageContent* createFromPacketPayload(FullMessageHeader const& messageHeader, QByteArray const& payload) const = 0;
 		};
 
 		template<typename T>
 		class TypedMessageContentFactory : public MessageContentFactory {
 		public:
+			virtual ~TypedMessageContentFactory() {}
+
 			virtual MessageContent* createFromPacketPayload(FullMessageHeader const& messageHeader, QByteArray const& payload) const override {
 				T t;
 				return t.fromPacketPayload(messageHeader, payload);
