74f2efca715f47792edb7542e2ddee381da659ce
Philipp Berger <berger@cs.rwth-aachen.de>
2020-02-20 13:16:01 +0100

    Some fixes for MacOSX

---
+++ share/macosx/Info.plist.cmake
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+  <key>NSPrincipalClass</key>
+  <string>NSApplication</string>
+  <key>CFBundleAllowMixedLocalizations</key>
+  <true/>
+  <key>CFBundleDevelopmentRegion</key>
+  <string>English</string>
+  <key>CFBundleDisplayName</key>
+  <string>${PROJECT_NAME}</string>
+  <key>CFBundleExecutable</key>
+  <string>${PROGNAME}</string>
+  <key>CFBundleIconFile</key>
+  <string>openmittsu.icns</string>
+  <key>CFBundleIdentifier</key>
+  <string>de.openmittsu.openmittsu</string>
+  <key>CFBundleInfoDictionaryVersion</key>
+  <string>6.0</string>
+  <key>CFBundleName</key>
+  <string>${PROJECT_NAME}</string>
+  <key>CFBundlePackageType</key>
+  <string>APPL</string>
+  <key>CFBundleShortVersionString</key>
+  <string>${OPENMITTSU_VERSION}</string>
+  <key>CFBundleSignature</key>
+  <string>????</string>
+  <key>CFBundleVersion</key>
+  <string>${OPENMITTSU_VERSION_NUM}</string>
+  <key>NSHumanReadableCopyright</key>
+  <string>Copyright 2015-2020 Philipp Berger</string>
+  </dict>
+</plist>
